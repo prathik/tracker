5b3fee9ca261d7441e3f60acc72ba8e9e22db9b6
Prathik M <prathik011@gmail.com>
2021-05-10 21:18:48 +0530

    Add ability to create work details

---
+++ repo/bolt_db_repo.go
@@ -0,0 +1,87 @@
+package repo
+
+import (
+	"bytes"
+	"github.com/boltdb/bolt"
+	"github.com/prathik/tracker/service"
+	"github.com/vmihailenco/msgpack/v5"
+	"log"
+	"time"
+)
+
+const timeFormat = time.RFC3339
+
+type boltDbRepo struct {
+	db *bolt.DB
+}
+
+
+func (b *boltDbRepo) GetWeekData() *service.WeekData {
+	dayCount := map[string]*service.DayData{}
+	_ = b.db.View(func(tx *bolt.Tx) error {
+		c := tx.Bucket([]byte("work")).Cursor()
+		now := time.Now()
+		min := []byte(now.AddDate(0, 0, -7).Format(timeFormat))
+		max := []byte(now.Format(timeFormat))
+
+		for k, v := c.Seek(min); k != nil && bytes.Compare(k, max) <= 0; k, v = c.Next() {
+			t, _ := time.Parse(timeFormat, string(k))
+			dayData := dayCount[t.Format("06-01-02")]
+			if dayData == nil {
+				dayData = &service.DayData{Time: t, Count: 0}
+			}
+			var item service.Item
+			err := msgpack.Unmarshal(v, &item)
+			if err != nil {
+				panic(err)
+			}
+			dayData.Count = dayData.Count + 1
+			dayData.WorkItem = append(dayData.WorkItem, &item)
+			dayCount[t.Format("06-01-02")] = dayData
+		}
+		return nil
+	})
+
+	var dayData []*service.DayData
+	for _, v := range dayCount {
+		dayData = append(dayData, v)
+	}
+
+	return &service.WeekData{WeekData: dayData}
+}
+
+func (b *boltDbRepo) Create(item *service.Item) {
+	err := b.db.Update(func(tx *bolt.Tx) error {
+		w, err := tx.CreateBucketIfNotExists([]byte("work"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		timeSlot := GetSlotFromTime(item.Time)
+		b, err := msgpack.Marshal(item)
+		if err != nil {
+			panic(err)
+		}
+		return w.Put(timeSlot, b)
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (b *boltDbRepo) Close() {
+	_ = b.db.Close()
+}
+
+func GetSlotFromTime(currentTime time.Time) []byte {
+	timeSlot := []byte(currentTime.Format(timeFormat))
+	return timeSlot
+}
+
+func NewBoltDbRepo() *boltDbRepo {
+	db, err := bolt.Open("work.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &boltDbRepo{db: db}
+}
---
+++ service/work_item.go
@@ -0,0 +1,24 @@
+package service
+
+import "time"
+
+// WeekData represents the work done in the week.
+type WeekData struct {
+	WeekData []*DayData
+}
+
+// DayData is a collection of work done in a day.
+type DayData struct {
+	Time     time.Time
+	Count    int
+	WorkItem []*Item
+}
+
+// Item represents a single work item.
+type Item struct {
+	Time       time.Time // Time when the item was done
+	Work       string // Work that was done
+	Joy        int // Joy in the Work
+	Importance int // Importance of the Work
+	Notes      string // Any learnings from the Work is captured here
+}
