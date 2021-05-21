10b80ae78331e06a2ab858d03517a170e861c1cd
Prathik Rajendran M <prathik011@gmail.com>
2021-05-21 17:31:32 +0530

    Fix show command description

--- cmd/show.go
+++ cmd/show.go
@@ -24,13 +24,7 @@ import (
 // showCmd represents the show command
 var showCmd = &cobra.Command{
 	Use:   "show",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Shows the activities done in the past week",
 	Run: func(cmd *cobra.Command, args []string) {
 		db := cmd.Flag("db").Value.String()
 		bolt := repo.NewBoltDbRepo(db)
