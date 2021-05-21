32192cb91cd26ff01e033b7e868b17ad83f8316e
Prathik Rajendran M <prathik011@gmail.com>
2021-05-21 08:40:05 +0530

    Add date into table and print with group by on date

--- cmd/util.go
+++ cmd/util.go
@@ -33,16 +33,17 @@ func PrintWeekData(ss *service.SessionService) {
 
 // PrintByDay prints the data per day with items of each day in a table
 func PrintByDay(ss *service.SessionService) {
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Date", "Work", "Joy", "Importance", "Notes"})
 	for _, d := range ss.QueryData(7).DayDataCollection {
-		color.Green("%s", d.Time.Format("2006-01-02"))
-		table := tablewriter.NewWriter(os.Stdout)
-		table.SetHeader([]string{"Work", "Joy", "Importance", "Notes"})
 		for _, wi := range d.WorkItem {
-			printData := []string{wi.Work, strconv.Itoa(wi.Joy), strconv.Itoa(wi.Importance), wi.Notes}
-			table.Rich(printData, []tablewriter.Colors{{}, getColour(wi.Joy), getColour(wi.Importance), {}})
+			printData := []string{d.Time.Format("2006-01-02"), wi.Work, strconv.Itoa(wi.Joy), strconv.Itoa(wi.Importance), wi.Notes}
+			table.Rich(printData, []tablewriter.Colors{{}, {}, getColour(wi.Joy), getColour(wi.Importance), {}})
 		}
-		table.Render()
+
 	}
+	table.SetAutoMergeCellsByColumnIndex([]int{0})
+	table.Render()
 }
 
 func getColour(value int) tablewriter.Colors {
