e677b8e34fccc7312d03d276c47036899a9b5d5a
Prathik Rajendran M <prathik011@gmail.com>
2021-12-22 16:30:20 +0530

    Remove unwanted stuff

--- cmd/capture.go
+++ cmd/capture.go
@@ -12,14 +12,8 @@ import (
 // captureCmd represents the capture command
 var captureCmd = &cobra.Command{
 	Use:   "capture",
-	Short: "A brief description of your command",
+	Short: "Capture an inbox item",
 	Args: cobra.ExactArgs(1),
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		db := cmd.Flag("db").Value.String()
 		bolt, err := repo.NewBoltDbRepo(db)
