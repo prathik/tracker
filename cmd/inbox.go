4da37eca83f77221a1330fbe528612c7b81410c4
Prathik Rajendran M <prathik011@gmail.com>
2021-12-22 16:30:49 +0530

    Remove unwanted stuff

--- cmd/inbox.go
+++ cmd/inbox.go
@@ -11,13 +11,7 @@ import (
 // inboxCmd represents the inbox command
 var inboxCmd = &cobra.Command{
 	Use:   "inbox",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Show inbox",
 	Run: func(cmd *cobra.Command, args []string) {
 		db := cmd.Flag("db").Value.String()
 		bolt, err := repo.NewBoltDbRepo(db)
